ef616e958ee503867085294eb4fbbecf64640912
Liv Cowden <liv@cowden.com>
2022-02-07 16:02:47 +0100

    Correct° min

--- src/Form/DishType.php
+++ src/Form/DishType.php
@@ -3,15 +3,16 @@
 namespace App\Form;
 
 use App\Entity\Dish;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Ingredient;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class DishType extends AbstractType
 {
